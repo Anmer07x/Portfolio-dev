6921bc854bb5a291b5a1a5db4ac6d7c83baa41f5
Anmer Dev <115746770+Anmer07x@users.noreply.github.com>
2025-09-04 19:41:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/ 
 export default defineConfig({   
+    base: "./",  // 👈 necesario para que Netlify cargue bien los assets
     plugins: [react()],   
     css: {
         postcss: {
